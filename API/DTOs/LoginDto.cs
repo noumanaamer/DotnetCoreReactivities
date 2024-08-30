da43faf71b7d860b8b6513e01e449020feedea21
noumanaamer <noumanaamer100@gmail.com>
2024-08-31 00:08:46 +0500

    End of section 12

---
+++ API/DTOs/LoginDto.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API.DTOs
+{
+    public class LoginDto
+    {
+        public string Email { get; set; }   
+        public string Password { get; set; }
+    }
+}

5a54119387efca15f519c23ce73e2e3bd012e8eb
noumanaamer <noumanaamer100@gmail.com>
2024-09-03 14:51:37 +0500

    End of section 17

--- client-app/src/app/models/profile.ts
+++ client-app/src/app/models/profile.ts
@@ -3,14 +3,21 @@ import { User } from "./user";
 export interface Profile {
     username: string;
     displayName: string;
-    Image?: string;
+    image?: string;
     bio?: string;
+    photos?: Photo[]
 }
 
 export class Profile implements Profile {
     constructor(user: User) {
         this.username = user.username;
         this.displayName = user.displayName;
-        this.Image = user.image;
+        this.image = user.image;
     }
+}
+
+export interface Photo {
+    id: string;
+    url: string;
+    isMain: boolean;
 }

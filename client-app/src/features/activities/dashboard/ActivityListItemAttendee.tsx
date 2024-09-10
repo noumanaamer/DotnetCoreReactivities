aa83dd961141b3af9de6a1629f6d49b9d1af092f
noumanaamer <noumanaamer100@gmail.com>
2024-09-10 14:41:01 +0500

    End of section 20

--- client-app/src/features/activities/dashboard/ActivityListItemAttendee.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItemAttendee.tsx
@@ -1,5 +1,4 @@
 import { observer } from "mobx-react-lite";
-import React from "react";
 import { Image, List, Popup } from "semantic-ui-react";
 import { Profile } from "../../../app/models/profile";
 import { Link } from "react-router-dom";
@@ -10,6 +9,11 @@ interface Props {
 }
 
 export default observer(function ActivityListItemAttendee({ attendees }: Props) {
+    const styles = {
+        borderColor: 'orange',
+        borderWidth: 3 
+    }
+
     return (
         <List horizontal>
             {attendees.map(attendee => (
@@ -18,7 +22,12 @@ export default observer(function ActivityListItemAttendee({ attendees }: Props) 
                     key={attendee.username}
                     trigger={
                         <List.Item key={attendee.username} as={Link} to={`/profiles/${attendee.username}`}>
-                            <Image size='mini' circular src={attendee.image || '/assets/user.png'} />
+                            <Image 
+                                size='mini' 
+                                circular src={attendee.image || '/assets/user.png'} 
+                                bordered
+                                style={attendee.following ? styles : null}
+                                />
                         </List.Item>
                     }
                 >
--- client-app/src/features/activities/details/ActivityDetailedSidebar.tsx
+++ client-app/src/features/activities/details/ActivityDetailedSidebar.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Segment, List, Label, Item, Image } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import { observer } from 'mobx-react-lite'
@@ -39,7 +38,8 @@ export default observer(function ActivityDetailedSidebar({ activity: { attendees
                                 <Item.Header as='h3'>
                                     <Link to={`/profiles/${attendee.username}`}>{attendee.displayName}</Link>
                                 </Item.Header>
-                                <Item.Extra style={{ color: 'orange' }}>Following</Item.Extra>
+                                {attendee.following && 
+                                <Item.Extra style={{ color: 'orange' }}>Following</Item.Extra>}
                             </Item.Content>
                         </Item>
                     ))}
